82a375287571f06695af4cd4b3f41df2d8a17881
Yang Peng <yp@ubuntu.(none)>
2015-03-01 23:20:43 -0500

    adding the extract a single category method

--- python/multi-modal/extract.py
+++ python/multi-modal/extract.py
@@ -1,14 +1,23 @@
 import urllib
-import json
+import simplejson
 
-query = 'cat'
-response = urllib.urlopen('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=cat').read()
-res = json.loads(response)
-#print res1
+def extractData(category):
+	# get top 200 queries for one category
+	num_queries = 50 * 4  
+	query = urllib.urlencode({'q' : category})
+	url = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + category
 
-moreUrl = res['responseData']['cursor']['moreResultsUrl']
-response1 = urllib.urlopen(moreUrl).read()
-#res1 = json.loads(response1)
-
-print response1
-
+	for start in range(0, num_queries, 4):
+    		request_url = '{0}&start={1}'.format(url, start)
+    		search_results = urllib.urlopen(request_url)
+    		json = simplejson.loads(search_results.read())
+    		if json['responseData'] != None:
+			#print json['responseData']['cursor']['pages']
+			#results = json['responseData']['results']
+    			#for i in results:
+        		#	print i['title'] + ": " + i['url']
+			print 
+		else:
+			print start
+extractData('cat')
+#extractData('dog')
