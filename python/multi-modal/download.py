138f514088d9c4b7136d3d76b991520ab9476a47
Yang Peng <pengyang823@gmail.com>
2015-03-04 15:58:03 -0500

    Google still blocks mechanize

--- python/multi-modal/download.py
+++ python/multi-modal/download.py
@@ -8,17 +8,22 @@ def getSearch(query):
 	browser = mechanize.Browser()
 	browser.set_handle_robots(False)
 	browser.addheaders = [('User-agent', 'chrome')]
-	url = 'https://www.google.com/search?source=lnms&tbm=isch&sa=X&q=' + query
+	url = 'https://www.google.com/search?q=' + query + '&es_sm=122&source=lnms&tbm=isch&sa=X&biw=1301&bih=611i'
 	print 'url: ', url
-	html = browser.open(url).read()
-	f = open('d.txt', 'w')
-	f.write(html)
-	img_urls = []
-	titles = []
-	res0 = html.split('imgurl')[1:]
-	print res0
-	for it in res0:
-		res1 = it.split('&'[0])[0]
-		print res1	
+	html = browser.open(url)
+	soup = BeautifulSoup(html)
+	print type(soup)
+	l = soup.find_all('img')
+	for x in l:
+		print x
+	#f = open('d.txt', 'w')
+	#f.write(soup)
+	#img_urls = []
+	#titles = []
+	#res0 = soup.split('imgurl')[1:]
+	#print res0
+	#for it in res0:
+	#	res1 = it.split('&'[0])[0]
+	#	print res1	
 	
 getSearch('cat')
---
+++ python/multi-modal/parse.py
@@ -0,0 +1,3 @@
+# parse the existing html downloaded from Google search by hand
+def parse():
+	print 'hello world'
