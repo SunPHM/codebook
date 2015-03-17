515d372adc97aedeecb1f5b36574a920889b593b
Yang Peng <pengyang823@gmail.com>
2015-03-17 17:02:08 -0400

    add some code for reading the files

--- python/multi-modal/clean.py
+++ python/multi-modal/clean.py
@@ -1,2 +1,23 @@
 # clean the dataset by removing unused title information and update the map.txt
+import os
 
+def clean(folder):
+	subfolders = os.listdir(folder)
+	for sf in subfolders:
+		files = os.listdir(folder + '/' + sf)
+		mt = folder + '/' + sf + '/map.txt'
+		m = mtToMap(mt)
+		print m
+		#for f in files:
+		#	if
+ 
+def mtToMap (mt):
+	f = open(mt, 'r')
+	m = dict()
+	index = 0
+	for line in f:
+		m[str(index) + '.jpg'] = line[0 : len(line) - 1]
+		index += 1
+	return m	
+
+clean('/home/hadoop/Downloads/mmd-2')
